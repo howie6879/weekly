f939adcf1dd34df14db7bcd5fa73ff9ffe7f6db2
howie6879 <howie6879@gmail.com>
2021-10-31 21:04:10 +0800

    Remove site dir

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-echo "> Building HTML by mddocs..."
-mkdocs build
+# echo "> Building HTML by mddocs..."
+# mkdocs build
 
 echo "> Push code to git"
 read -p "> Please input commit info: " commit_info
 git add *
-git commit -m "Auto push: $commit_info"
+git commit -m "$commit_info"
 git push
-# mkdocs gh-deploy --clean
+mkdocs gh-deploy --clean
 echo "> Done!"
