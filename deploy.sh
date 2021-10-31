d4d9357f89b250a8b1f9e50e86a6894b5689245b
howie6879 <howie6879@gmail.com>
2021-10-31 20:56:54 +0800

    Auto push: Fix title for 001

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo "> Building HTML by mddocs..."
 mkdocs build
 
 echo "> Push code to git"
-read -p "> please input commit info: " commit_info
+read -p "> Please input commit info: " commit_info
 git add *
 git commit -m "Auto push: $commit_info"
 git push
