169930207cb869cfae14aef996d8aed2dc6a536e
howie6879 <howie6879@gmail.com>
2021-12-31 15:35:11 +0800

    Update: 2021 weekly

---
+++ src/gen_pdf.py
@@ -0,0 +1,6 @@
+"""
+    Created by howie.hu at 2021-12-31.
+    Description: 周刊生成PDF https://github.com/simonhaenisch/md-to-pdf
+        - 命令：npm i -g md-to-pdf 
+    Changelog: all notable changes to this file will be documented
+"""
