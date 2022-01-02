8d8326f0d2029f8e8c5a7feb267cd501fbab53ba
howie.hu <howie6879@gmail.com>
2022-01-02 09:40:07 +0800

    Update: pdf

--- src/gen_pdf.py
+++ src/gen_pdf.py
@@ -2,5 +2,7 @@
     Created by howie.hu at 2021-12-31.
     Description: 周刊生成PDF https://github.com/simonhaenisch/md-to-pdf
         - 命令：npm i -g md-to-pdf 
+        - md2pdf ./weekly/archive/2021/2021_weekly_all.md  --config-file./weekly/src/config.json
+
     Changelog: all notable changes to this file will be documented
 """
