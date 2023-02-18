d2d0071f5cb19166647b006383eb3c4af5d87975
胡宇 <huyu@amz123.com>
2023-02-18 12:34:50 +0800

    Deployed d8f8b6d6 with MkDocs version: 1.3.0

--- statics/js/tongji.bd.js
+++
@@ -1,9 +0,0 @@
-<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?3a0fb3a244ca15f90db09f159830c217";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>
