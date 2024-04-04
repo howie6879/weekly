27537d6465d7b29454b75ee109341d93cb0e8a2a
howie6879 <howie6879@gmail.com>
2024-04-04 18:31:19 +0800

    Deployed e3ee35f with MkDocs version: 1.5.2

---
+++ statics/js/custom.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // 检查localStorage是否已设置
+  var isPopupClosed = localStorage.getItem("popupClosed");
+  var currentDate = new Date();
+  if (isPopupClosed) {
+    var closedDate = new Date(isPopupClosed);
+    if (currentDate - closedDate < 86400000) {
+      return;
+    }
+  }
+
+  var popup = document.createElement("div");
+  popup.id = "my-popup";
+
+  // 创建关闭按钮
+  var closeButton = document.createElement("span");
+  closeButton.textContent = "X";
+  closeButton.className = "close-btn";
+  popup.appendChild(closeButton);
+
+  // 创建链接元素
+  var link = document.createElement("a");
+  // link.href = "https://www.fre123.com/weekly?from=weekly";
+  // link.textContent = "🔥 老胡精选周刊大全>>>";
+
+  link.href =
+    "https://developer.aliyun.com/topic/lingma/activities/202403?taskCode=14508&recordId=3a279f7d6dd399e15ca29242cce86a5c#/?utm_content=m_fission_1";
+  link.textContent = "👉 通义灵码·体验AI编码&盲盒，快来薅羊毛吧>>>";
+
+  link.target = "_blank";
+  popup.appendChild(link);
+  document.body.appendChild(popup);
+
+  setTimeout(function () {
+    popup.classList.add("show");
+  }, 500);
+
+  //关闭按钮事件
+  closeButton.addEventListener("click", function (event) {
+    event.stopPropagation(); // 阻止事件冒泡
+    popup.classList.remove("show");
+    localStorage.setItem("popupClosed", new Date().toISOString()); // 设置关闭时间
+  });
+});
