734ab98178f67b33dc640a70183d1af17432f28c
howie6879 <howie6879@gmail.com>
2025-04-07 22:35:15 +0800

    Add 183

--- docs/statics/js/custom.js
+++ docs/statics/js/custom.js
@@ -48,24 +48,24 @@ document.addEventListener("DOMContentLoaded", function () {
   script.setAttribute("data-website-id", "eef10855-77dc-4398-b380-cba46664665e");
   document.head.appendChild(script);
 
-  var script2 = document.createElement("script");
-  script2.type = "text/javascript";
-  script2.charset = "UTF-8";
-  script2.src = "https://cdn.wwads.cn/js/makemoney.js";
-  script2.async = true;
-  document.head.appendChild(script2);
+  // var script2 = document.createElement("script");
+  // script2.type = "text/javascript";
+  // script2.charset = "UTF-8";
+  // script2.src = "https://cdn.wwads.cn/js/makemoney.js";
+  // script2.async = true;
+  // document.head.appendChild(script2);
 
-  // 创建一个新的 div 元素
-  var newDiv = document.createElement("div");
-  // 设置 div 的类名、数据属性和样式
-  newDiv.className = "wwads-cn wwads-horizontal wwads-sticky";
-  newDiv.setAttribute("data-id", "357");
-  newDiv.style.maxWidth = "350px";
+  // // 创建一个新的 div 元素
+  // var newDiv = document.createElement("div");
+  // // 设置 div 的类名、数据属性和样式
+  // newDiv.className = "wwads-cn wwads-horizontal wwads-sticky";
+  // newDiv.setAttribute("data-id", "357");
+  // newDiv.style.maxWidth = "350px";
 
-  // 获取 body 的第一个子元素
-  var firstChild = document.body.firstChild;
+  // // 获取 body 的第一个子元素
+  // var firstChild = document.body.firstChild;
 
-  // 将新创建的 div 插入到 body 的第一行
-  document.body.insertBefore(newDiv, firstChild);
+  // // 将新创建的 div 插入到 body 的第一行
+  // document.body.insertBefore(newDiv, firstChild);
 
 });
